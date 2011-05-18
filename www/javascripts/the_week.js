0e57bc83666e040fe850367bb313d22fc1ce89ba
Scott Turnquest <sturnque@sturnque-MacBook-Pro.local>
2011-05-18 17:22:04 +0100

    $1

--- www/javascripts/the_week.js
+++ www/javascripts/the_week.js
@@ -1,8 +1,13 @@
 function swipe_page(to_page_id, slide_right_to_left) {
+    change_page(to_page_id, slide_right_to_left, 'slide');
+
+}
+
+function change_page(to_page_id, reverse_transition, transition) {
     // JQuery mobile does not work properly for changing pages to an internal div.
     // See: http://forum.jquery.com/topic/changepage-not-updating-hash-for-internal-div-pages
     // To workaround supply a all parameters for page transition.
-    $.mobile.changePage(to_page_id, 'slide', slide_right_to_left, true);
+    $.mobile.changePage(to_page_id, transition, reverse_transition, true);
     e.stopImmediatePropagation();
     return false;
 }
@@ -38,7 +43,37 @@ $(function() {
     });
 
     $('#contents-bookmark').click(function() {
-        alert('Bookmarks: ' + localStorage["bookmarks"])
+
+        $('#bookmark-container .bookmark-row').remove();
+
+        var bookmarks = localStorage["bookmarks"];
+        if (bookmarks != null) {
+            var pages = bookmarks.split(",");
+            for (var i = 0; i < pages.length; i++) {
+                var bookmarkRow = $('#bookmark_template').clone();
+                var bookmarkLink = bookmarkRow.find('.bookmark-title');
+                bookmarkLink.text(pages[i]);
+                bookmarkLink.attr('href', '#' + pages[i]);
+                bookmarkRow.appendTo('#bookmark-container');
+            }
+        }
+
+        $('.remove-bookmark').click(function() {
+            var bookMarkPageId = $(this).prev().text();
+            $(this).parent().remove();
+            var existingBookmarks = localStorage["bookmarks"].split(',');
+            var remainingBookmarks = '';
+            for (var i = 0; i < existingBookmarks.length; i++) {
+                var existingBookmarkPageId = existingBookmarks[i];
+                if (bookMarkPageId != existingBookmarkPageId) {
+                    remainingBookmarks += existingBookmarkPageId + ',';
+                }
+            }
+
+            localStorage["bookmarks"] = remainingBookmarks;
+        });
+
+        change_page('#bookmarks', false, 'slideup');
     });
 
 });
